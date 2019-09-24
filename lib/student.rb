848c353a98b14ea55eaf5d05c1765502647f4d51
Chantel Lowe <chantellowe17@gmail.com>
2019-09-24 16:18:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
   
   def hello
-    p "Hey there! I'm so excited to learn stuff."
+    p "Hey there! I'm so excited to learn stuff.\n"
   end
   
   def raise_hand
-    p "Pick me!"
+    p "Pick me!\n"
   end
   
 end
